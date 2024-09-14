7786e67dd4c92cc658c2081d3ab6a8813b633de6
Ángel Díaz <diazangel9311@gmail.com>
2024-09-14 12:13:21 -0500

    Show balance in current budget

--- src/app/home/@currentbudget/BudgetInfo.tsx
+++ src/app/home/@currentbudget/BudgetInfo.tsx
@@ -2,8 +2,9 @@ import { FullBudget } from '@/types/budget';
 
 export default function BudgetInfo({ budget }: { budget: FullBudget }) {
   return (
-    <div>
-      {budget.month}/{budget.year}
-    </div>
+    <section>
+      <h2>{budget.month}/{budget.year}</h2>
+      <p>{budget.balance}</p>
+    </section>
   );
 }
