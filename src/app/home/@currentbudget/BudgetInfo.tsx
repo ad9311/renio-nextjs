8f69d2e7fbbdc3d207406cff308b491df762ff49
Ángel Díaz <diazangel9311@gmail.com>
2024-09-14 12:13:39 -0500

    Fix liting errors

--- src/app/home/@currentbudget/BudgetInfo.tsx
+++ src/app/home/@currentbudget/BudgetInfo.tsx
@@ -3,7 +3,9 @@ import { FullBudget } from '@/types/budget';
 export default function BudgetInfo({ budget }: { budget: FullBudget }) {
   return (
     <section>
-      <h2>{budget.month}/{budget.year}</h2>
+      <h2>
+        {budget.month}/{budget.year}
+      </h2>
       <p>{budget.balance}</p>
     </section>
   );
