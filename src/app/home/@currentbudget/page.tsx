c5c1be70553b1a4a8f4912f1a24a3ac6c04a2ffb
Ángel Díaz <diazangel9311@gmail.com>
2024-09-14 11:46:49 -0500

    Add query params to include expenses and income list

--- src/app/home/@currentbudget/page.tsx
+++ src/app/home/@currentbudget/page.tsx
@@ -1,7 +1,7 @@
 import { getResource } from '@/helpers/fetch';
 
 export default async function CurrentBudgetPage() {
-  const response = await getResource(`${process.env.API}/budgets/current`);
+  const response = await getResource(`${process.env.API}/budgets/current?transactions=income:expenses`);
   if (response.ok) {
     const json = await response.json();
     return <div>{JSON.stringify(json)}</div>;
