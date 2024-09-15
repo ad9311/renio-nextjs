0b71135b18b5697a0e0c1d65e58c58ba46cf21ae
Ángel Díaz <diazangel9311@gmail.com>
2024-09-15 12:21:06 -0500

    Improve sign in page loading images

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -4,12 +4,12 @@ import SignInForm from './SignInForm';
 
 export default function SignInPage() {
   return (
-    <div className="relative translate-y-40 md:translate-y-36">
+    <div className="pt-40 md:pt-36">
       <div className="mx-auto w-96 md:hidden">
-        <Image src="/img/renio-short.svg" alt="renio" width={1} height={1} className="w-28" />
+        <Image src="/img/renio-short.svg" alt="renio" width={1} height={1} className="w-28" priority />
       </div>
       <div className="mx-auto w-fit hidden md:block">
-        <Image src="/img/renio-full.svg" alt="renio" width={1} height={1} className="w-96" />
+        <Image src="/img/renio-full.svg" alt="renio" width={1} height={1} className="w-96" priority />
       </div>
       <div className="mt-4 md:mt-10 mx-auto card max-w-96">
         <SignInForm />
