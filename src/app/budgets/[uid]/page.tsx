453f8a7d78d94a80f16b545b23ca657f656d0ae7
Ángel Díaz <diazangel9311@gmail.com>
2024-09-14 19:57:59 -0500

    Update packages

--- src/app/budgets/[uid]/page.tsx
+++ src/app/budgets/[uid]/page.tsx
@@ -1,5 +1,3 @@
 export default function BudgetPage() {
-  return (
-    <div>page</div>
-  )
+  return <div>page</div>;
 }
--- src/app/budgets/page.tsx
+++ src/app/budgets/page.tsx
@@ -1,5 +1,3 @@
 export default function BudgetsPage() {
-  return (
-    <div>page</div>
-  )
+  return <div>page</div>;
 }
