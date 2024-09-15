68e8a3320ce4ed333c3084eba64ecc0d5357dff2
Ángel Díaz <diazangel9311@gmail.com>
2024-09-15 12:43:02 -0500

    Fix linting errors

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -6,10 +6,24 @@ export default function SignInPage() {
   return (
     <div className="pt-40 md:pt-36">
       <div className="mx-auto w-96 md:hidden">
-        <Image src="/img/renio-short.svg" alt="renio" width={1} height={1} className="w-28" priority />
+        <Image
+          src="/img/renio-short.svg"
+          alt="renio"
+          width={1}
+          height={1}
+          className="w-28"
+          priority
+        />
       </div>
       <div className="mx-auto w-fit hidden md:block">
-        <Image src="/img/renio-full.svg" alt="renio" width={1} height={1} className="w-96" priority />
+        <Image
+          src="/img/renio-full.svg"
+          alt="renio"
+          width={1}
+          height={1}
+          className="w-96"
+          priority
+        />
       </div>
       <div className="mt-4 md:mt-10 mx-auto card max-w-96">
         <SignInForm />
