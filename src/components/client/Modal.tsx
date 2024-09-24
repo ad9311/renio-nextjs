4f45054831086c750574ed012fafdf47e0f73e6f
Ángel Díaz <diazangel9311@gmail.com>
2024-09-24 16:20:39 -0500

    Adjust modal height

--- src/components/client/Modal.tsx
+++ src/components/client/Modal.tsx
@@ -3,7 +3,7 @@
 export default function Modal({ children, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props} className="fixed top-0 bottom-0 left-0 right-0 bg-gray-500/35">
-      <div className="card max-w-96 mx-auto mt-60">{children}</div>
+      <div className="card max-w-96 mx-auto mt-40">{children}</div>
     </div>
   );
 }
